6c6b1448c129d6712db078595ccf38a9c877f2cb
Giovanni Briceño - Vinni <g.briceno@uniandes.edu.co>
2023-03-05 16:02:56 +0000

    "Adiciona componentes de docker "

--- entregasalpes/api/__init__.py
+++ entregasalpes/api/__init__.py
@@ -5,28 +5,57 @@ from flask_swagger import swagger
 # Identifica el directorio base
 basedir = os.path.abspath(os.path.dirname(__file__))
 
+
 def importar_modelos_alchemy():
     import entregasalpes.modulos.ordenes.infraestructura.dto
+
+def registrar_handlers():
+    import entregasalpes.modulos.ordenes.aplicacion
+
+def comenzar_consumidor(app):
+    """
+    Este es un código de ejemplo. Aunque esto sea funcional puede ser un poco peligroso tener 
+    threads corriendo por si solos. Mi sugerencia es en estos casos usar un verdadero manejador
+    de procesos y threads como Celery.
+    """
+
+    import threading
+    import entregasalpes.modulos.ordenes.infraestructura.consumidores as cliente    
+
+    # Suscripción a eventos
+    threading.Thread(target=ordenes.suscribirse_a_eventos, args=[app]).start()
+
+    # Suscripción a comandos
+    threading.Thread(target=ordenes.suscribirse_a_comandos, args=[app]).start()
+
+
 
 def create_app(configuracion=None):
     # Init la aplicacion de Flask
     app = Flask(__name__, instance_relative_config=True)
 
-    # Configuracion de BD
-    app.config['SQLALCHEMY_DATABASE_URI'] =\
-            'sqlite:///' + os.path.join(basedir, 'database.db')
-    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+    app.config['SESSION_TYPE'] = 'filesystem'
+    app.config['TESTING'] = configuracion.get('TESTING')
 
      # Inicializa la DB
-    from entregasalpes.config.db import init_db
+    from entregasalpes.config.db import init_db, database_connection
+
+    app.config['SQLALCHEMY_DATABASE_URI'] = database_connection(configuracion, basedir=basedir)
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
     init_db(app)
 
+     # Inicializa la DB
     from entregasalpes.config.db import db
 
+
     importar_modelos_alchemy()
+    registrar_handlers()
 
     with app.app_context():
         db.create_all()
+        if not app.config.get('TESTING'):
+            comenzar_consumidor(app)
 
      # Importa Blueprints
     from . import ordenes
@@ -40,6 +69,14 @@ def create_app(configuracion=None):
         swag['info']['version'] = "1.0"
         swag['info']['title'] = "My API"
         return jsonify(swag)
+
+    @app.route("/spec")
+    def spec():
+        swag = swagger(app)
+        swag['info']['version'] = "1.0"
+        swag['info']['title'] = "My API"
+        return jsonify(swag)
+
 
     @app.route("/health")
     def health():
