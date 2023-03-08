a027af73ac1db6878cfa0cda135ace56194f62fe
Juan Acevedo <j.acevedob@uniandes.edu.co>
2023-03-08 03:27:08 +0000

    Merge principal

--- init_terceros.sql
+++ init_terceros.sql
@@ -5,8 +5,3 @@ CREATE TABLE hoja_ruta(
     bodega_centro VARCHAR(255),
     id_orden VARCHAR(255)
 ) COMMENT 'Tabla hoja de ruta';
-CREATE TABLE ordenes(  
-    id int NOT NULL PRIMARY KEY AUTO_INCREMENT COMMENT 'Llave primaria',
-    fecha_creacion VARCHAR(10),
-    id_cliente VARCHAR(255)
-) COMMENT 'Tabla de ordenes';
