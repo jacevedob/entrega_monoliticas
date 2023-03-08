09c2b8e819d22f09faffa31606027d0c3522b712
Juan Acevedo <j.acevedob@uniandes.edu.co>
2023-03-08 01:16:21 +0000

    Se envia mensaje

--- entregasalpes/terceros/productor.py
+++ entregasalpes/terceros/productor.py
@@ -6,10 +6,8 @@ def send_topic(topic):
     load_dotenv()
     token = os.getenv('PULSAR_TOKEN')
     service_url = os.getenv('PULSAR_URL') 
-    producer_pulsar = os.getenv('PULSAR_PRODUCER') 
+    producer_pulsar = os.getenv('PULSAR_PRODUCER_TERCEROS') 
     client = pulsar.Client(service_url, authentication=pulsar.AuthenticationToken(token))
     producer = client.create_producer(producer_pulsar)
     producer.send((topic).encode('utf-8'))
     client.close()
-
-send_topic("Topico de prueba manual")
