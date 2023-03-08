273ca325a8a7bfca2d5543d976fe03df3b799cfa
Juan Acevedo <j.acevedob@uniandes.edu.co>
2023-03-08 02:04:48 +0000

    Revert "cambios"
    This reverts commit 0d05df4a6066093b544666a9b863c35d9f4de37f.

--- entregasalpes/terceros/consumidor.py
+++ entregasalpes/terceros/consumidor.py
@@ -1,20 +1,20 @@
 import pulsar
-#import insert
-#import productor
+import insert
+import productor
 import os
 from dotenv import load_dotenv
 
 load_dotenv()
 token = os.getenv('PULSAR_TOKEN')
 service_url = os.getenv('PULSAR_URL') 
-pulsar_consumer = os.getenv('PULSAR_CONSUMER_PEDIDOS') 
+pulsar_consumer = os.getenv('PULSAR_CONSUMER_TERCEROS') 
 client = pulsar.Client(service_url, authentication=pulsar.AuthenticationToken(token))
 consumer = client.subscribe(pulsar_consumer, 'test-subscription')
 
 while True:
-    mensaje = consumer.receive()
-    datos = mensaje.data()
-    print(f'Evento recibido: {datos}')
-    consumer.acknowledge(mensaje)     
+    msg = consumer.receive()
+    insert.insert_db()
+    productor.send_topic('enviar_recogida')
+    consumer.acknowledge(msg)
 
 client.close()
