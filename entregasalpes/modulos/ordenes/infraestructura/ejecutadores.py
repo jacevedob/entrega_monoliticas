8440ae4952b0179abd8bb319ac89eb4cd5c1d36a
Giovanni Briceño - Vinni <g.briceno@uniandes.edu.co>
2023-03-15 15:09:39 +0000

    ejecutador

---
+++ entregasalpes/modulos/ordenes/infraestructura/ejecutadores.py
@@ -0,0 +1,46 @@
+import mysql.connector
+import os
+from dotenv import load_dotenv
+from entregasalpes.modulos.ordenes.dominio.entidades import Orden
+from entregasalpes.modulos.ordenes.aplicacion.dto import ProductoDTO, OrdenDTO
+
+load_dotenv()
+hostname = os.getenv('DB_HOSTNAME', default="localhost")
+user = os.getenv('USER_DB', default="localhost")
+password = os.getenv('PASSWORD_DB')
+database = os.getenv('DATABASE_ORDENES', default="ordenes")
+
+def registra_orden(orden: OrdenDTO):
+  try:
+    mydb = mysql.connector.connect(
+      host = hostname,
+      user = user,
+      password = password,
+      database = database,
+      port=3306
+    )
+  except mysql.connector.Error as err:
+    print("Something went wrong: {}".format(err))
+
+  print("------------------------------------------------------->",orden.id, '',orden.id_cliente, '',orden.fecha_creacion);
+
+  mycursor = mydb.cursor()
+  sql = "INSERT INTO ordenes (id, id_cliente, fecha_creacion ) VALUES (%s, %s, %s)"
+  val = (orden.id, orden.id_cliente, orden.fecha_creacion)
+  mycursor.execute(sql, val)
+  mydb.commit()
+  print(mycursor.rowcount, "registro insertado orden")
+
+  print(mycursor.rowcount, "registro insertado")
+  print("mis prod - - - - -", orden.productos)
+
+
+  for producto in orden.productos:
+    print ('sxxxxxxxxxxxxxxxxxxxxxx Serial xxxxxxxxxxxxxx ', producto[0].serial)
+    sql = "INSERT INTO productos (serial, descripcion, precio, fecha_vencimiento ) VALUES (%s, %s, %s, %s)"
+    val = (str(producto[0].serial), str(producto[0].descripcion), int(producto[0].precio), str(producto[0].fecha_vencimiento) )
+    mycursor.execute(sql, val)
+    mydb.commit()
+  
+
+  print(mycursor.rowcount, "registro insertado productos")
