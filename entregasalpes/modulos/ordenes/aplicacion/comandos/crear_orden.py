4d8c15ec4cdad3901a33e538ecb7fe1b63c15228
Giovanni Briceño - Vinni <g.briceno@uniandes.edu.co>
2023-03-15 23:56:00 -0500

    Actualza envio ordenes.

--- entregasalpes/modulos/ordenes/aplicacion/comandos/crear_orden.py
+++ entregasalpes/modulos/ordenes/aplicacion/comandos/crear_orden.py
@@ -38,16 +38,9 @@ class CrearOrdenHandler(CrearOrdenBaseHandler):
         id: str = field(default_factory=str)
         productos: list[ProductoDTO] = field(default_factory=list)
         orden: Orden = self.fabrica_compras.crear_objeto(orden_dto, MapeadorOrden())
-        load_dotenv()
-        token = os.getenv('PULSAR_TOKEN')
-        service_url = os.getenv('PULSAR_URL') 
-        producer_pulsar = os.getenv('PULSAR_PROD_ORDENES') 
-
-        client = pulsar.Client(service_url, authentication=pulsar.AuthenticationToken(token))
-        producer = client.create_producer(producer_pulsar)
         
-        producer.send((str(orden_dto)).encode('utf-8'))
-        client.close() 
+        
+        
 
         repositorio = self.fabrica_repositorio.crear_objeto(RepositorioOrdenes)
         repositorio_eventos = self.fabrica_repositorio.crear_objeto(RepositorioEventosOrdenes)
@@ -64,15 +57,18 @@ class CrearOrdenHandler(CrearOrdenBaseHandler):
         if resultado != 'error_o':
             self.enviaSagas(orden_dto.id, resultado)
 
-        #send_topic(orden_dto)
+        if resultado == 'success':            
+        #Envia el topico   
+            load_dotenv()
+            token = os.getenv('PULSAR_TOKEN')
+            service_url = os.getenv('PULSAR_URL') 
+            producer_pulsar = os.getenv('PULSAR_PROD_ORDENES') 
 
-        #orden: Orden = self.fabrica_compras.crear_objeto(orden_dto, MapeadorOrden())
-        #orden.crear_orden(orden)
+            client = pulsar.Client(service_url, authentication=pulsar.AuthenticationToken(token))
+            producer = client.create_producer(producer_pulsar)        
+            producer.send((str(orden_dto)).encode('utf-8'))
+            client.close() 
 
-        
-
-        #UnidadTrabajoPuerto.registrar_batch(repositorio.agregar, orden, repositorio_eventos_func=repositorio_eventos.agregar)
-        #UnidadTrabajoPuerto.commit()
 
     def enviaSagas(self, id: str, status: str):
         print('Enviando a sagas ', id)
